2f6f6bcacdef3a0c282cf5bc0b98ee7d06417b1f
snowbawler <78890148+snowbawler@users.noreply.github.com>
2021-12-04 11:37:19 -0600

    does not work

--- src/LivestockLineup.java
+++ src/LivestockLineup.java
@@ -26,7 +26,7 @@ public class LivestockLineup {
 			pairs.get(i).add(st.nextToken()); System.out.println(pairs.get(i));
 		}
 		
-
+System.out.println();
 		
 		int sto = 0;
 		int sto1 = 0;
@@ -36,7 +36,7 @@ public class LivestockLineup {
 				else if(pairs.get(i).contains(pairs.get(j).get(1))) {sto = 1; sto1 = 0;}
 				else break;
 				if(pairs.get(i).get(0).equals(pairs.get(j).get(sto1))) pairs.get(i).add(0, pairs.get(j).get(sto));
-				else pairs.get(i).add(pairs.get(j).get(sto));
+				else pairs.get(i).add(pairs.get(j).get(sto1));
 				pairs.remove(j);
 				j--;
 			}
@@ -46,16 +46,48 @@ public class LivestockLineup {
 			System.out.print(pairs.get(i));
 		}
 		
-		ArrayList<String> ret = new ArrayList<>();
+System.out.println();
+		
+		ArrayList<String> store = new ArrayList<>();
 		for(int i=0; i<pairs.size(); i++) {
 			if(pairs.get(i).get(0).compareTo(pairs.get(i).get(pairs.get(i).size()-1))>0) {
 				for(int j = pairs.get(i).size()-1; j>=0; j--) {
-					ret.add(pairs.get(i).get(j));
+					store.add(pairs.get(i).get(j));
 				}
+				pairs.get(i).clear();
+				pairs.get(i).addAll(store);
+			}
+			store.clear();
+		}
+		
+		for(int i=0; i<pairs.size(); i++) {
+			System.out.print(pairs.get(i));
+		}
+System.out.println();
+		
+		ArrayList<String> ret = new ArrayList<>();
+		boolean contain = false;
+		
+		for(int i=0; i<s.length; i++) { 
+			if(!ret.contains(s[i])) {
+				for(int j=0; j<pairs.size(); j++) {
+					if(pairs.get(j).get(0).equals(s[i])) {
+							ret.addAll(pairs.get(j));
+					}
+					if(pairs.get(j).contains(s[i])) contain = true;
+				}
+				if(!contain) {
+					ret.add(s[i]);	
+				}
+				contain = false;
 			}
 		}
 		
 		System.out.print(ret);
+		
+		for(int i=0; i<ret.size(); i++) {
+			pw.println(ret.get(i));
+		}
 		
 		pw.close();
 		br.close();
